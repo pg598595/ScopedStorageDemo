1fe35cd32cfc0bab49a87d2f9d59dc2718e6c194
Priyanka Gupta <priyanka.gupta@solutionanalysts.com>
2020-01-09 14:46:44 +0530

    edit in Media Store

--- app/src/main/java/com/example/socpedstoragedemo/const/CheckPemission.kt
+++ app/src/main/java/com/example/socpedstoragedemo/const/CheckPemission.kt
@@ -8,6 +8,7 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.socpedstoragedemo.const.Constants.PERMISSION_READ_EXTERNAL_STORAGE
 import com.example.socpedstoragedemo.const.Constants.PERMISSION_REQUEST_CODE
+import java.util.ArrayList
 
 
 object CheckPemission {
@@ -46,6 +47,7 @@ object CheckPemission {
                 context,
                 android.Manifest.permission.READ_EXTERNAL_STORAGE
             )
+
         return result == PackageManager.PERMISSION_GRANTED
     }
 
@@ -56,12 +58,34 @@ object CheckPemission {
 
         ActivityCompat.requestPermissions(
             context as Activity,
-            arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSION_READ_EXTERNAL_STORAGE
+            arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE,android.Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSION_READ_EXTERNAL_STORAGE
         )
 
     }
 
+    fun checkMultiplePermissions(permissionList: ArrayList<String>,context: Context): Boolean {
+        val arrPermission = checkPermissions(permissionList,context)
+        if (arrPermission.isNotEmpty()) {
+            ActivityCompat.requestPermissions(context as Activity,
+                arrPermission,
+                PERMISSION_READ_EXTERNAL_STORAGE)
+            return true
+        }
+        return false
+    }
 
+    private fun checkPermissions(permissionList: ArrayList<String>,context: Context): Array<String?> {
+        val arrPermission = ArrayList<String>()
+
+        for (i in permissionList.indices) {
+            val permission = permissionList[i]
+            if (ContextCompat.checkSelfPermission(context,
+                    permission) != PackageManager.PERMISSION_GRANTED) {
+                arrPermission.add(permission)
+            }
+        }
+        return arrPermission.toTypedArray()
+    }
 
 
 }
--- app/src/main/java/com/example/socpedstoragedemo/downlaoddemo/DownloadImageActivity.kt
+++ app/src/main/java/com/example/socpedstoragedemo/downlaoddemo/DownloadImageActivity.kt
@@ -4,7 +4,6 @@ import android.app.DownloadManager
 import android.content.ContentValues
 import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Build
@@ -30,6 +29,8 @@ class DownloadImageActivity : AppCompatActivity() {
     var imgUrl = "https://cdn.pixabay.com/photo/2014/04/14/20/11/japanese-cherry-trees-324175__340.jpg"
 
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_download_image)
@@ -41,12 +42,14 @@ class DownloadImageActivity : AppCompatActivity() {
             .dontAnimate()
             .into(ivImageDownload)
 
+
+
     }
 
     fun downloadImage(view: View) {
 
 
-        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
 
 
             if (checkPermissionForReadWrite(this)) {
@@ -58,44 +61,6 @@ class DownloadImageActivity : AppCompatActivity() {
 
         } else {
             downloadFile(imgUrl)
-        }
-
-    }
-
-    fun saveFileToLocalFolder(view: View) {
-        if (checkPermissionForReadWrite(this)) {
-            try {
-
-
-                val bitmap: Bitmap = (ivImageDownload.drawable as BitmapDrawable).bitmap
-
-                val filepath = saveToInternalStorage(bitmap)
-
-
-
-
-
-                Toast.makeText(
-                    applicationContext,
-                    "Download successfully to $filepath",
-                    Toast.LENGTH_LONG
-                ).show()
-
-
-            } catch (e: Exception) {
-                Toast.makeText(
-                    applicationContext,
-                    "$e=== EACCES (Permission denied)",
-                    Toast.LENGTH_LONG
-                ).show()
-                Log.i("TAG", e.toString())
-                e.printStackTrace()
-
-            }
-
-
-        } else {
-            requestPermissionForReadWrite(this)
         }
 
     }
@@ -129,7 +94,57 @@ class DownloadImageActivity : AppCompatActivity() {
     }
 
 
-    private fun saveToInternalStorage(bitmapImage: Bitmap): String {
+    fun saveFileToLocalFolder(view: View) {
+
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            if (checkPermissionForReadWrite(this)) {
+                downloadtoLocalFolder()
+            } else {
+                requestPermissionForReadWrite(this)
+            }
+
+        }
+        else{
+            downloadtoLocalFolder()
+
+        }
+
+    }
+
+    private fun downloadtoLocalFolder(){
+        try {
+
+            var bitmap = (ivImageDownload.drawable as BitmapDrawable).bitmap
+
+            val filepath = saveToInternalStorage(bitmap)
+
+
+
+
+
+            Toast.makeText(
+                applicationContext,
+                "Download successfully to $filepath",
+                Toast.LENGTH_LONG
+            ).show()
+
+
+        } catch (e: Exception) {
+            Toast.makeText(
+                applicationContext,
+                "$e=== EACCES (Permission denied)",
+                Toast.LENGTH_LONG
+            ).show()
+            Log.i("TAG", e.toString())
+            e.printStackTrace()
+
+        }
+    }
+
+
+
+
+    private fun saveToInternalStorage(bitmapImage: Bitmap?): String {
 
         var filepath = ""
 
@@ -148,7 +163,7 @@ class DownloadImageActivity : AppCompatActivity() {
 
             fos = FileOutputStream(testFile)
 
-            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos)
+            bitmapImage?.compress(Bitmap.CompressFormat.PNG, 100, fos)
 
         } catch (e: IOException) {
             filepath = "not done"
@@ -162,40 +177,88 @@ class DownloadImageActivity : AppCompatActivity() {
     fun downloadImageWithMediaStore(view: View){
 
         try{
-            val bitmap: Bitmap = (ivImageDownload.drawable as BitmapDrawable).bitmap
-            val values = ContentValues().apply{
-                put(MediaStore.Images.Media.DISPLAY_NAME,"imgMS.jpeg")
-                put(MediaStore.Images.Media.MIME_TYPE,"image/jpeg")
-                put(MediaStore.Images.Media.IS_PENDING,1)	}
 
-            val resolver = contentResolver
+            var bitmap = (ivImageDownload.drawable as BitmapDrawable).bitmap
 
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
 
-            val collection = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
-
-
-            val item = resolver.insert(collection,values)
+//                Toast.makeText(
+//                        applicationContext,
+//                        "support in ANDROID 10",
+//                        Toast.LENGTH_LONG
+//                    ).show()
 
 
-            if (item != null) {
-                resolver.openOutputStream(item).use { out ->
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out)
+                val file =  File(getExternalFilesDir(null), "DemoFile.jpg")
+
+                try {
+
+                    var fos: FileOutputStream? = null
+
+                    fos = FileOutputStream(file)
+
+                    bitmap?.compress(Bitmap.CompressFormat.PNG, 100, fos)
+
+                    Toast.makeText(
+                        applicationContext,
+                        "Download successfully to  ${file.path}",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                } catch ( e:IOException) {
+                    // Unable to create file, likely because external storage is
+                    // not currently mounted.
+                    Log.i("ExternalStorage", "Error writing $file", e)
+                    Toast.makeText(
+                        applicationContext,
+                        "Error writing ${file.path}",
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
-                values.clear()
-                values.put(MediaStore.Images.Media.IS_PENDING,0)
-                resolver.update(item,values,null,null)
 
-                Toast.makeText(
-                    applicationContext,
-                    "Download successfully to ${item.path}",
-                    Toast.LENGTH_LONG
-                ).show()
+
+
+            }
+            else{
+
+
+                val values = ContentValues().apply{
+                    put(MediaStore.Images.Media.DISPLAY_NAME,"imgMS.jpeg")
+                    put(MediaStore.Images.Media.MIME_TYPE,"image/jpeg")
+                    put(MediaStore.Images.Media.IS_PENDING,1)	}
+
+                val resolver = contentResolver
+
+
+                val collection = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+
+
+                val item = resolver.insert(collection,values)
+
+
+                if (item != null) {
+                    resolver.openOutputStream(item).use { out ->
+                        bitmap?.compress(Bitmap.CompressFormat.JPEG, 90, out)
+                    }
+                    values.clear()
+                    values.put(MediaStore.Images.Media.IS_PENDING,0)
+                    resolver.update(item,values,null,null)
+
+                    Toast.makeText(
+                        applicationContext,
+                        "Download successfully to ${item.path}",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+
+                }
 
 
             }
 
         }
         catch (e:Exception){
+            Log.i("Tag","$e")
             Toast.makeText(
                 applicationContext,
                 "$e",
@@ -209,51 +272,80 @@ class DownloadImageActivity : AppCompatActivity() {
 
     fun downloadImageWithMediaStoreToSdCard(view: View){
 
-        try{
-            val bitmap: Bitmap = (ivImageDownload.drawable as BitmapDrawable).bitmap
-            val values = ContentValues().apply{
-                put(MediaStore.Images.Media.DISPLAY_NAME,"imgMS.jpeg")
-                put(MediaStore.Images.Media.MIME_TYPE,"image/jpeg")
-                put(MediaStore.Images.Media.IS_PENDING,1)	}
-
-            val resolver = contentResolver
-           // Log.i("TAG","${MediaStore.getExternalVolumeNames(this)}")
-
-            val volumeNames = MediaStore.getExternalVolumeNames(this)
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.Q){
 
 
-            val collection = MediaStore.Images.Media.getContentUri(volumeNames.elementAt(1))
+            try {
+                var bitmap = (ivImageDownload.drawable as BitmapDrawable).bitmap
 
+                    var path = Environment.getExternalStorageDirectory().toString()
 
-            val item = resolver.insert(collection,values)
+                    var filename =  File("$path/image.jpg")
 
+                    var out =  FileOutputStream(filename)
 
-            if (item != null) {
-                resolver.openOutputStream(item).use { out ->
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out)
+                    bitmap?.compress(Bitmap.CompressFormat.JPEG, 90, out)
+                    out.flush()
+                    out.close()
+
+                    MediaStore.Images.Media.insertImage(contentResolver, filename.absolutePath, filename.name, filename.name)
+
+                    Toast.makeText(applicationContext, "Saved in  $filename", Toast.LENGTH_LONG).show()
+                } catch ( e:Exception) {
+                    e.printStackTrace()
                 }
-                values.clear()
-                values.put(MediaStore.Images.Media.IS_PENDING,0)
-                resolver.update(item,values,null,null)
 
+
+        }
+        else{
+            try{
+                var bitmap = (ivImageDownload.drawable as BitmapDrawable).bitmap
+                val values = ContentValues().apply{
+                    put(MediaStore.Images.Media.DISPLAY_NAME,"imgMS.jpeg")
+                    put(MediaStore.Images.Media.MIME_TYPE,"image/jpeg")
+                    put(MediaStore.Images.Media.IS_PENDING,1)	}
+
+                val resolver = contentResolver
+                // Log.i("TAG","${MediaStore.getExternalVolumeNames(this)}")
+
+                val volumeNames = MediaStore.getExternalVolumeNames(this)
+
+
+                val collection = MediaStore.Images.Media.getContentUri(volumeNames.elementAt(1))
+
+
+                val item = resolver.insert(collection,values)
+
+
+                if (item != null) {
+                    resolver.openOutputStream(item).use { out ->
+                        bitmap?.compress(Bitmap.CompressFormat.JPEG, 90, out)
+                    }
+                    values.clear()
+                    values.put(MediaStore.Images.Media.IS_PENDING,0)
+                    resolver.update(item,values,null,null)
+
+                    Toast.makeText(
+                        applicationContext,
+                        "Download successfully to ${item.path}",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+
+                }
+
+            }
+            catch (e:Exception){
                 Toast.makeText(
                     applicationContext,
-                    "Download successfully to ${item.path}",
+                    "$e",
                     Toast.LENGTH_LONG
                 ).show()
-
 
             }
 
         }
-        catch (e:Exception){
-            Toast.makeText(
-                applicationContext,
-                "$e",
-                Toast.LENGTH_LONG
-            ).show()
 
-        }
 
     }
 
@@ -261,6 +353,46 @@ class DownloadImageActivity : AppCompatActivity() {
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //    fun pdf(view: View) {
